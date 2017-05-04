d894dd103a1e9a47a22115d91341f54f69f4a82c
Peng Wang <pwang.online@gmail.com>
2017-05-03 23:40:11 -0700

    add Sign Language Recognizer project

--- Planning/tests/test_my_air_cargo_problems.py
+++ Planning/tests/test_my_air_cargo_problems.py
@@ -1,5 +1,6 @@
 import os
 import sys
+
 parent = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(os.path.dirname(parent), "aimacode"))
 from aimacode.planning import Action
