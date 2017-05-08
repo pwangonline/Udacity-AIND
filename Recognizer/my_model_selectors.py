e8ca4bcd469a3c49ef4836d0ffd3b88c025ac6e4
Peng Wang <pwang.online@gmail.com>
2017-05-08 14:32:15 -0700

    add selector cv

--- Recognizer/my_model_selectors.py
+++ Recognizer/my_model_selectors.py
@@ -9,100 +9,123 @@ from asl_utils import combine_sequences
 
 
 class ModelSelector(object):
-    '''
-    base class for model selection (strategy design pattern)
-    '''
+	'''
+	base class for model selection (strategy design pattern)
+	'''
 
-    def __init__(self, all_word_sequences: dict, all_word_Xlengths: dict, this_word: str,
-                 n_constant=3,
-                 min_n_components=2, max_n_components=10,
-                 random_state=14, verbose=False):
-        self.words = all_word_sequences
-        self.hwords = all_word_Xlengths
-        self.sequences = all_word_sequences[this_word]
-        self.X, self.lengths = all_word_Xlengths[this_word]
-        self.this_word = this_word
-        self.n_constant = n_constant
-        self.min_n_components = min_n_components
-        self.max_n_components = max_n_components
-        self.random_state = random_state
-        self.verbose = verbose
+	def __init__(self, all_word_sequences: dict, all_word_Xlengths: dict, this_word: str,
+				 n_constant=3,
+				 min_n_components=2, max_n_components=10,
+				 random_state=14, verbose=False):
+		self.words = all_word_sequences
+		self.hwords = all_word_Xlengths
+		self.sequences = all_word_sequences[this_word]
+		self.X, self.lengths = all_word_Xlengths[this_word]
+		self.this_word = this_word
+		self.n_constant = n_constant
+		self.min_n_components = min_n_components
+		self.max_n_components = max_n_components
+		self.random_state = random_state
+		self.verbose = verbose
 
-    def select(self):
-        raise NotImplementedError
+	def select(self):
+		raise NotImplementedError
 
-    def base_model(self, num_states):
-        # with warnings.catch_warnings():
-        warnings.filterwarnings("ignore", category=DeprecationWarning)
-        # warnings.filterwarnings("ignore", category=RuntimeWarning)
-        try:
-            hmm_model = GaussianHMM(n_components=num_states, covariance_type="diag", n_iter=1000,
-                                    random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
-            if self.verbose:
-                print("model created for {} with {} states".format(self.this_word, num_states))
-            return hmm_model
-        except:
-            if self.verbose:
-                print("failure on {} with {} states".format(self.this_word, num_states))
-            return None
+	def base_model(self, num_states):
+		# with warnings.catch_warnings():
+		warnings.filterwarnings("ignore", category=DeprecationWarning)
+		# warnings.filterwarnings("ignore", category=RuntimeWarning)
+		try:
+			hmm_model = GaussianHMM(n_components=num_states, covariance_type="diag", n_iter=1000,
+									random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
+			if self.verbose:
+				print("model created for {} with {} states".format(self.this_word, num_states))
+			return hmm_model
+		except:
+			if self.verbose:
+				print("failure on {} with {} states".format(self.this_word, num_states))
+			return None
 
 
 class SelectorConstant(ModelSelector):
-    """ select the model with value self.n_constant
+	""" select the model with value self.n_constant
 
-    """
+	"""
 
-    def select(self):
-        """ select based on n_constant value
+	def select(self):
+		""" select based on n_constant value
 
-        :return: GaussianHMM object
-        """
-        best_num_components = self.n_constant
-        return self.base_model(best_num_components)
+		:return: GaussianHMM object
+		"""
+		best_num_components = self.n_constant
+		return self.base_model(best_num_components)
 
 
 class SelectorBIC(ModelSelector):
-    """ select the model with the lowest Baysian Information Criterion(BIC) score
+	""" select the model with the lowest Baysian Information Criterion(BIC) score
 
-    http://www2.imm.dtu.dk/courses/02433/doc/ch6_slides.pdf
-    Bayesian information criteria: BIC = -2 * logL + p * logN
-    """
+	http://www2.imm.dtu.dk/courses/02433/doc/ch6_slides.pdf
+	Bayesian information criteria: BIC = -2 * logL + p * logN
+	"""
 
-    def select(self):
-        """ select the best model for self.this_word based on
-        BIC score for n between self.min_n_components and self.max_n_components
+	def select(self):
+		""" select the best model for self.this_word based on
+		BIC score for n between self.min_n_components and self.max_n_components
 
-        :return: GaussianHMM object
-        """
-        warnings.filterwarnings("ignore", category=DeprecationWarning)
+		:return: GaussianHMM object
+		"""
+		warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+		# TODO implement model selection based on BIC scores
+		raise NotImplementedError
 
 
 class SelectorDIC(ModelSelector):
-    ''' select best model based on Discriminative Information Criterion
+	''' select best model based on Discriminative Information Criterion
 
-    Biem, Alain. "A model selection criterion for classification: Application to hmm topology optimization."
-    Document Analysis and Recognition, 2003. Proceedings. Seventh International Conference on. IEEE, 2003.
-    http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.58.6208&rep=rep1&type=pdf
-    DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
-    '''
+	Biem, Alain. "A model selection criterion for classification: Application to hmm topology optimization."
+	Document Analysis and Recognition, 2003. Proceedings. Seventh International Conference on. IEEE, 2003.
+	http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.58.6208&rep=rep1&type=pdf
+	DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
+	'''
 
-    def select(self):
-        warnings.filterwarnings("ignore", category=DeprecationWarning)
+	def select(self):
+		warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+		# TODO implement model selection based on DIC scores
+		raise NotImplementedError
 
 
 class SelectorCV(ModelSelector):
-    ''' select best model based on average log Likelihood of cross-validation folds
+	''' select best model based on average log Likelihood of cross-validation folds
 
-    '''
+	'''
 
-    def select(self):
-        warnings.filterwarnings("ignore", category=DeprecationWarning)
+	def select(self):
+		warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection using CV
-        raise NotImplementedError
+		# TODO implement model selection using CV
+		# raise NotImplementedError
+		bestLogL = float("-inf")
+		bestModel = None
+		bestComp = None
+		for n_comp in range(self.min_n_components, self.max_n_components):
+			logLs = []
+			if n_comp > len(self.sequences):
+				continue
+			split_method = KFold(n_splits=n_comp)
+			for cv_train_idx, cv_test_idx in split_method.split(self.sequences):
+				train_X, train_lengths = combine_sequences(cv_train_idx, self.sequences)
+				model = GaussianHMM(n_components=n_comp, covariance_type="diag", n_iter=1000,
+				            random_state=self.random_state, verbose=False).fit(train_X, train_lengths)
+				test_X, test_lengths = combine_sequences(cv_test_idx, self.sequences)
+				logLs.append(model.score(test_X, test_lengths))
+			logL = sum(logLs) / float(len(logLs))
+			if logL > bestLogL:
+				bestLogL = logL
+				bestComp = n_comp
+		bestModel = GaussianHMM(n_components=bestComp, covariance_type="diag", n_iter=1000, random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
+		print(self.this_word)
+		print(bestLogL)
+		return bestModel
+
